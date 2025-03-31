544727a71be612e7a532e7738c54a53c7d82630e
Himani Joshi <150302069+HIMANI2918@users.noreply.github.com>
2025-03-31 15:17:26 -0500

    Update ROC.R

--- ROC.R
+++ ROC.R
@@ -18,10 +18,12 @@ library(parallelMap)
 
 ```{r}
 library(pROC)
+#If using two algorithm random forest(rf) and support vector machine(svm)
 roc_rose <- plot(roc(predSVM$data$truth, predSVM$data$prob.Heat_Stress), print.auc = TRUE, col = "blue", 
                  lty=1,print.auc.cex=1.2)
 roc_rose2 <- plot(roc(rfPred$data$truth, rfPred$data$prob.Heat_Stress), print.auc = TRUE, 
                   col = "green", print.auc.y = .4, add = TRUE, lty=2, print.auc.cex=1.2)
+
 legend("topleft",
        legend=c("SVM", "RF"),
        col=c("blue", "green"),
@@ -30,5 +32,34 @@ legend("topleft",
 
 #Do these two auc values differ?
 print(roc.test(roc_rose, roc_rose2))
+
+
+#Prediction model using only one algorithm, for example, random forest(rf)
+
+roc_rose <- plot(roc(rfPred2$data$truth, rfPred2$data$prob.Heat_Stress), 
+                 print.auc = TRUE, col = "blue", lty=1, 
+                 print.auc.x = 0.8, print.auc.y = 0.6, print.auc.cex=1.2)     #Model 1
+roc_rose2 <- plot(roc(rfPred3$data$truth, rfPred3$data$prob.Heat_Stress), 
+                  print.auc = TRUE, col = "red", lty=2, add = TRUE, 
+                  print.auc.x = 0.8, print.auc.y = 0.5, print.auc.cex=1.2)    #Model 2
+
+roc_rose3 <- plot(roc(rfPred$data$truth, rfPred$data$prob.Heat_Stress), 
+                  print.auc = TRUE, col = "green", lty=2, add = TRUE, 
+                  print.auc.x = 0.8, print.auc.y = 0.4, print.auc.cex=1.2)    #Model 3
+
+
+legend("topleft",
+       legend=c("Model 1", "Model 2", "Model 3"),
+       col=c("blue", "red", "green"),
+       lty = c(1,2),
+       lwd=2, 
+       cex =0.7, 
+       xpd = TRUE, 
+       horiz = TRUE)
+
+#Do these two auc values differ?
+print(roc.test(roc_rose, roc_rose2))
+print(roc.test(roc_rose, roc_rose3))
+print(roc.test(roc_rose2, roc_rose3))
 ```
 
